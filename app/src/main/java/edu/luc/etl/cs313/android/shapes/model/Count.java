7891571359839d3434114eec24a82a5b029222b5
melanie-ab <mabarca1@luc.edu>
2026-03-22 11:35:10 -0500

    Update Count.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Count.java
@@ -4,47 +4,49 @@ package edu.luc.etl.cs313.android.shapes.model;
  * A visitor to compute the number of basic shapes in a (possibly complex)
  * shape.
  */
-public class Count implements Visitor<Integer> {
-
-    // TODO entirely your job
+public class Count implements Visitor {
 
     @Override
     public Integer onPolygon(final Polygon p) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onCircle(final Circle c) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onGroup(final Group g) {
-        return -1;
+        int total = 0;
+        for (final Object s : g.getShapes()) {
+            total += ((Shape) s).accept(this).intValue();
+        }
+        return total;
     }
 
     @Override
     public Integer onRectangle(final Rectangle q) {
-        return -1;
+        return 1;
     }
 
     @Override
     public Integer onOutline(final Outline o) {
-        return -1;
+        return o.getShape().accept(this).intValue();
     }
 
     @Override
     public Integer onFill(final Fill c) {
-        return -1;
+        return c.getShape().accept(this).intValue();
     }
 
     @Override
     public Integer onLocation(final Location l) {
-        return -1;
+        return l.getShape().accept(this).intValue();
     }
 
     @Override
     public Integer onStrokeColor(final StrokeColor c) {
-        return -1;
+        return c.getShape().accept(this).intValue();
     }
 }
