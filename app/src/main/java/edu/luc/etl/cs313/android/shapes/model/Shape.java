1749b631152ff4bb844826fc92e189f6d1b1b869
melanie-ab <mabarca1@luc.edu>
2026-03-22 12:11:43 -0500

    Update Shape.java

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Shape.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Shape.java
@@ -4,5 +4,6 @@ package edu.luc.etl.cs313.android.shapes.model;
  * A graphical shape.
  */
 public sealed interface Shape permits Circle, Rectangle, Group, StrokeColor, Fill, Location, Outline {
+
     <Result> Result accept(Visitor<Result> v);
 }
